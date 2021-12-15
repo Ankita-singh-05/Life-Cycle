89cb36485d03b389d95e3f7c615f880a8cd5b901
Ankita-singh-05 <asankitasingh2017@gmail.com>
2021-12-15 23:39:42 +0530

    Hooks in React

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App() {
   return (
     <div>
       <User name="Ankita Singh" description="Software Engineer"/>
-      <User name="Ankita Singh" description="Software Engineer"/>
-      <User name="Ankita Singh" description="Software Engineer"/>
-      <User name="Ankita Singh" description="Software Engineer"/>
-      <User name="Ankita Singh" description="Software Engineer"/>
     </div>
   );
 }
 
 export default App;
+
+//Hooks -- it was introduced to enable the use of state in the functional component
+//useState -- it is used to update the state
+//useEffect -- putting in some logic or heavy computations
--- src/components/user.js
+++ src/components/user.js
@@ -1,36 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-class User extends React.Component {
+const User = (props) => {
+  const [planet, setPlanet] = useState("Earth");
+
+  //componentDidMount
+  useEffect(() => {
+    console.log("Component Mounting");
+  }, [] );
+
+  //componentDidUpdate
+  useEffect(()=> {
+    console.log("Planet Changes");
+  }, [planet]);
+
+  return (
+    <div>
+    <h1>{props.name}</h1>
+    <p>{props.description}</p>
+    <button onClick={() => setPlanet("Pluto")}>{planet}</button>
+    </div>
+  );
+};
+
+export default User;
+ /* class User extends React.Component {
   constructor (props) {
     super(props);  // super method is used to call the parent class i.e constructor
 
       //a state is a set of data that an individual components can hold
       this.state = {
-        user: "Student"
+        planet: "Earth"
       };
       console.log("Hey I am from Constructor!");
-  }
+  } */
 
-  componentDidMount(){
+  /* componentDidMount() {
     this.setState({user: "Undergrad Student"})
     console.log("Hey I am from ComponentDidMount!");
-  }
+  } */
 
-  render() { //logic happens2
+  /* render() { //logic happens2
     console.log("Hey I am from render!");
     return (
-      <div>
-        {/* to refer to the user class we need to write this keyword to access the things */}
-        <h1>{this.props.name}</h1>
+      <div> */
+        {/* to refer to the user class we need to write this keyword to access the things  */}
+        /*<h1>{this.props.name}</h1>
         <h2>{this.props.description}</h2>
         <h3>{this.state.user}</h3>
       </div>
     );
   };
-};
-
-
-export default User;
+}; */
+//export default User;
 
 
 
